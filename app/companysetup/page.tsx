bb3dc4f3d161d56391d496b56f8405e040d79401
Ram Charan <thiriguduramcharan@gmail.com>
2026-07-02 10:17:01 +0530

    Fix company setup page

---
+++ app/companysetup/page.tsx
@@ -0,0 +1,8 @@
+export default function CompanySetupPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Company Setup</h1>
+      <p>Coming Soon...</p>
+    </div>
+  );
+}
