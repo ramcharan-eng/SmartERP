5554af2a70bcd3a3309081aea980d81ef149b9d8
Ram Charan <thiriguduramcharan@gmail.com>
2026-07-04 19:52:25 +0530

    Fix inventory and remove broken eslint config

--- app/inventory/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function InventoryPage() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">Inventory</h1>
-      <p>Coming Soon...</p>
-    </div>
-  );
-}
